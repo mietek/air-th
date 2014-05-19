2e7d5ee598b8ac2dbcdc02e87487627ae93338e2
Jinjing Wang <nfjinjing@gmail.com>
2014-05-19 23:21:45 +0800

    fix import

--- src/Air/TH.hs
+++ src/Air/TH.hs
@@ -1,12 +1,11 @@
 module Air.TH (
-    module Air.Data.Record.SimpleLabel
+    module Air.Data.Record.SimpleLabel.TH
   , module Air.TH.Here
   , module Air.TH.Default
   , module Air.TH.Air
 ) where
 
-import Air.Data.Record.SimpleLabel
+import Air.Data.Record.SimpleLabel.TH
 import Air.TH.Here
 import Air.TH.Default
 import Air.TH.Air
-
